2cb5b6fbbad34651cd4ed826d96bb3fb487325e5
Squid300 <squid400@gmail.com>
2022-06-30 20:14:52 -0400

    updated associations and added code to pull needed data from associations

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,19 +1,17 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Project, User, Task, User_tasks } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const projectData = await Project.findAll({
-        model: User,
-        attributes: ['name'],
-    });
+    const projectData = await Project.findAll({});
 
     const projects = projectData.map((project) => project.get({ plain: true }));
 
     res.render('homepage', {
       projects,
       logged_in: req.session.logged_in,
+      is_manager: req.session.is_manager
     });
   } catch (e) {
     res.status(500).json(e);
@@ -22,20 +20,22 @@ router.get('/', withAuth, async (req, res) => {
 
 router.get('/project/:id', async (req, res) => {
   try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
+    const projectData = await Project.findByPk(req.params.id);
+    const taskData = await Task.findAll({ 
+      where: { project_id: req.params.id },
+      include: {
+        model:  User, as: "UsersForTasks",
+      }
     });
 
     const project = projectData.get({ plain: true });
+    const task = taskData.map((task) => task.get({ plain: true }));
 
     res.render('project', {
       ...project,
-      logged_in: req.session.logged_in
+      ...task,
+      logged_in: req.session.logged_in,
+      is_manager: req.session.is_manager
     });
   } catch (err) {
     res.status(500).json(err);
@@ -53,7 +53,8 @@ router.get('/dev', withAuth, async (req, res) => {
 
     res.render('dev', {
       ...user,
-      logged_in: true
+      logged_in: req.session.logged_in,
+      is_manager: req.session.is_manager
     });
   } catch (err) {
     res.status(500).json(err);
